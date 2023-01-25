269bc6ce24318d68ec616869a1e50781cacd563b
Tub3rs08 <122375187+AnonLotusVNPC@users.noreply.github.com>
2023-01-25 20:18:19 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+apt install whois
+apt install nmap
+apt install ncat
+apt install nslookup
+apt install dnsutils
+apt install curl
+wpet https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+chmod +x *
+./bin.sh
